e6929649ab9a61b6f51515e5697aaf33a18c7f84
ssh <kathishivaram@gmail.com>
2019-04-11 21:52:34 +0530

    adding log in for user

---
+++ Flash-Chat/Controller/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  Flash-Chat
+//
+//  Created by apple on 4/11/19.
+//  Copyright © 2019 shivaapple. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error !=  nil {
+                print("Wrong Details \(error)")
+            }else{
+                self.performSegue(withIdentifier:"goToChat", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
